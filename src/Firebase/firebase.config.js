eb043d75b490745f63de0b46b807eb814bef92a6
swarga500 <swarga500@gmail.com>
2021-10-12 20:23:36 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCqgz4c3Kh5aIn99Hph87cpv3XSIAqLjoo",
+    authDomain: "react-firebase-authentic-2e103.firebaseapp.com",
+    projectId: "react-firebase-authentic-2e103",
+    storageBucket: "react-firebase-authentic-2e103.appspot.com",
+    messagingSenderId: "675233600394",
+    appId: "1:675233600394:web:929028f956a0f89a916e73"
+  };
+  export default firebaseConfig;
